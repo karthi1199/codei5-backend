e974807d4bcced0519b9494b3307a783a90ca404
dev-karthi <karthi@cloudi5.com>
2024-08-15 19:58:33 +0530

    cors error fixed again

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const fileUpload = require('express-fileupload');
-app.use(cors({origin: 'http://localhost:3000'}));
+app.use(cors({origin: ['http://localhost:3000'], credentials: true}));
 app.use(express.json());
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
